9414b1b6a9aa2f17098c4a262ec585a0ebfa34e5
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2025-01-28 19:57:38 +0100

    License headers added to files

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,9 @@
-﻿using SixLabors.ImageSharp;
+﻿// --------------------------------------------------------------------------
+// Copyright (c) 2024-2025 Ruzsinszki Gábor
+// This code is licensed under MIT license (see LICENSE for details)
+// --------------------------------------------------------------------------
+
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
 using Webmaster442.WindowsTerminal;
