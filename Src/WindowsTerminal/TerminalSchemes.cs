fa7e0785e9da35d989a387bad4fb59584f133919
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2025-02-18 21:13:54 +0100

    Added more themes

--- Src/WindowsTerminal/TerminalSchemes.cs
+++ Src/WindowsTerminal/TerminalSchemes.cs
@@ -40,6 +40,7 @@ public static class TerminalSchemes
     public const string TangoLight = "Tango Light";
 
     /// <summary>
+    /// Purplepeter color scheme
     /// https://windowsterminalthemes.dev/?theme=purplepeter
     /// </summary>
     public static readonly TerminalScheme PurplepeterShecme = new()
@@ -68,6 +69,7 @@ public static class TerminalSchemes
     };
 
     /// <summary>
+    /// Github inspired color scheme
     /// https://windowsterminalthemes.dev/?theme=Github
     /// </summary>
     public static readonly TerminalScheme GithubShecme = new()
@@ -93,5 +95,90 @@ public static class TerminalSchemes
         Foreground = "#3e3e3e",
         SelectionBackground = "#a9c1e2",
         CursorColor = "#3f3f3f"
+    };
+
+    /// <summary>
+    /// Dracula color scheme
+    /// https://windowsterminalthemes.dev/?theme=Dracula
+    /// </summary>
+    public static readonly TerminalScheme Dracula = new()
+    {
+        Name = "Dracula",
+        Black = "#000000",
+        Red = "#ff5555",
+        Green = "#50fa7b",
+        Yellow = "#f1fa8c",
+        Blue = "#bd93f9",
+        Purple = "#ff79c6",
+        Cyan = "#8be9fd",
+        White = "#bbbbbb",
+        BrightBlack = "#555555",
+        BrightRed = "#ff5555",
+        BrightGreen = "#50fa7b",
+        BrightYellow = "#f1fa8c",
+        BrightBlue = "#bd93f9",
+        BrightPurple = "#ff79c6",
+        BrightCyan = "#8be9fd",
+        BrightWhite = "#ffffff",
+        Background = "#1e1f29",
+        Foreground = "#f8f8f2",
+        SelectionBackground = "#44475a",
+        CursorColor = "#bbbbbb"
+    };
+
+    /// <summary>
+    /// Retrowave color scheme
+    /// https://windowsterminalthemes.dev/?theme=Retrowave
+    /// </summary>
+    public static readonly TerminalScheme Retrowave = new()
+    {
+        Name = "Retrowave",
+        Black = "#181A1F",
+        Red = "#FF16B0",
+        Green = "#929292",
+        Yellow = "#fcee54",
+        Blue = "#46BDFF",
+        Purple = "#FF92DF",
+        Cyan = "#df81fc",
+        White = "#FFFFFF",
+        BrightBlack = "#FF16B0",
+        BrightRed = "#f85353",
+        BrightGreen = "#fcee54",
+        BrightYellow = "#ffffff",
+        BrightBlue = "#46BDFF",
+        BrightPurple = "#FF92DF",
+        BrightCyan = "#ff901f",
+        BrightWhite = "#ffffff",
+        Background = "#070825",
+        Foreground = "#46BDFF"
+    };
+
+    /// <summary>
+    /// Coffe theme color scheme
+    /// https://windowsterminalthemes.dev/?theme=coffee_theme
+    /// </summary>
+    public static readonly TerminalScheme CoffeTheme = new()
+    {
+        Name = "coffee_theme",
+        Black = "#000000",
+        Red = "#c91b00",
+        Green = "#00c200",
+        Yellow = "#c7c400",
+        Blue = "#0225c7",
+        Purple = "#ca30c7",
+        Cyan = "#00c5c7",
+        White = "#c7c7c7",
+        BrightBlack = "#686868",
+        BrightRed = "#ff6e67",
+        BrightGreen = "#5ffa68",
+        BrightYellow = "#fffc67",
+        BrightBlue = "#6871ff",
+        BrightPurple = "#ff77ff",
+        BrightCyan = "#60fdff",
+        BrightWhite = "#ffffff",
+        Background = "#f5deb3",
+        Foreground = "#000000",
+        SelectionBackground = "#c1deff",
+        CursorColor = "#c7c7c7"
     };
 }
